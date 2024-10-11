be9f7f7174f6060108d15beb8125c22634547bde
Zhanuzak Kudaibergenov <kudaibergenov.zhanuzak@gmail.com>
2024-10-11 17:45:58 +0500

    dockerfile with compiledeamon

--- internal/http/controllers/account_controller.go
+++ internal/http/controllers/account_controller.go
@@ -3,5 +3,5 @@ package controllers
 import "github.com/gofiber/fiber/v2"
 
 func GetAccounts(c *fiber.Ctx) error {
-	return c.SendString("Hello, Zhanuzak!")
+	return c.SendString("Hello, Zhanuzak Kudaibergenov!")
 }
